9ee1b4c0efae7f017c5c01f2cd30464e85e211f4
wilootours <wilooservice@gmail.com>
2025-04-02 11:06:33 +0530

    update navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import {logo} from "../../assets/index"
-import { navLinksdata } from '../../constants';
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaGithub,
+  FaSkype,
+} from "react-icons/fa";
+import { logo } from "../../assets/index";
+import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
@@ -44,7 +50,6 @@ const Navbar = () => {
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
                 <img className="w-32" src={logo} alt="logo" />
-                
               </div>
               <ul className="flex flex-col gap-4">
                 {navLinksdata.map((item) => (
@@ -71,15 +76,27 @@ const Navbar = () => {
                   Find me in
                 </h2>
                 <div className="flex gap-4">
-                  <span className="bannerIcon">
-                    <FaFacebookF />
-                  </span>
-                  <span className="bannerIcon">
-                    <FaTwitter />
-                  </span>
-                  <span className="bannerIcon">
+                  <a
+                    href="https://www.linkedin.com/in/nihal-das-7288b8205/"
+                    target="_blank"
+                    className="bannerIcon"
+                  >
                     <FaLinkedinIn />
-                  </span>
+                  </a>
+                  <a
+                    href="https://github.com/nihuGit?tab=repositories"
+                    target="_blank"
+                    className="bannerIcon"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    href="skype:live:.cid.ae7477c767d8b20b?chat"
+                    target="_blank"
+                    className="bannerIcon"
+                  >
+                    <FaSkype />
+                  </a>
                 </div>
               </div>
               <span
@@ -94,6 +111,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
